433f5f63b7f36f3645a979864c8ec9866b1d19cf
denikn <deniknugr@gmail.com>
2017-05-25 17:26:39 +0700

    Add files via upload

---
+++ IVELS-Schedule-master/app/src/main/java/com/ivelsproject/ivelsschedule/fragment/ScheduleFragment.java
@@ -0,0 +1,29 @@
+package com.ivelsproject.ivelsschedule.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ivelsproject.ivelsschedule.R;
+
+public class ScheduleFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_schedule, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.menu_schedule);
+        }
+    }
+}
